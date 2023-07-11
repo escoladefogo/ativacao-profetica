a3725c464f15ddb5c9936116f9d5f38c1e352206
Adriano Filgueiras <adriano.filgueiras@animaeducacao.com.br>
2023-07-11 14:17:53 -0300

    Adicionando campos ao formulário com interações do usuário, adicionando tela de informações iniciais, criação inicial da logo do evento

---
+++ script.js
@@ -0,0 +1,43 @@
+function Form(){
+   document.getElementById('text').style.cssText = 'display: none;';
+   document.getElementById('form').style.cssText = 'display: block;';
+}
+function Ministerio(resposta){
+    if (resposta == "Outro") {
+        document.getElementById('ministerio').removeAttribute("hidden");
+    }else{
+        document.getElementById('ministerio').setAttribute("hidden","true");
+    };
+}
+function Criancas(resposta){
+    if (resposta == "Sim") {
+        document.getElementById('criancas').removeAttribute("hidden");
+    }else{
+        document.getElementById('criancas').setAttribute("hidden","true");
+    };
+}
+
+function Gravar(nome, telefone){
+
+    fetch("https://sheetdb.io/api/v1/y6wzxu5btyj7l", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        data: [
+            {
+                'Nome': nome,
+                'Telefone': telefone,
+            }
+        ]
+    }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log("Success:", data);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
+ }
